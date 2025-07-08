b32702608acff49bf0eda52fd184d64820fa4af6
Kirill <eddy-95@mail.ru>
2025-07-08 11:43:14 +0300

    task1 compleate

---
+++ mongo-sharding/scripts/router-init.sh
@@ -0,0 +1,19 @@
+docker compose exec -T mongos_router mongosh --port 27020 <<EOF
+
+sh.addShard( "shard1/shard1:27018");
+sh.addShard( "shard2/shard2:27019");
+
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+
+use somedb
+
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
+
+db.helloDoc.countDocuments() 
+EOF
+
+docker compose exec -T shard1 mongosh --port 27018 <<EOF
+use somedb
+db.helloDoc.countDocuments()
+EOF
